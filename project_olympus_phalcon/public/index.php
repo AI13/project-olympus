3ac46ab0e23c3b621175e7796036549f8c5f557c
Thanapong Aroonroj <alieng30@gmail.com>
2015-10-20 13:48:27 +0700

    add config for each environment, include testing

--- project_olympus_phalcon/public/index.php
+++ project_olympus_phalcon/public/index.php
@@ -1,25 +1,65 @@
 <?php
 
 error_reporting(E_ALL);
-
-define('APP_PATH', realpath('..'));
+define('PHALCON_START', microtime(true));
 
 try {
+
+    defined('APP_PATH') || define('APP_PATH', realpath(dirname(__FILE__)) . '/../app');
+    defined('BASE_PATH') || define('BASE_PATH', realpath(dirname(__FILE__)) . '/..');
 
     /**
      * Read the configuration
      */
-    $config = include APP_PATH . "/app/config/config.php";
+    $config = include __DIR__ . "/../app/config/config.php";
+
+    $default_env = 'production';
+    $envMapping  = include APP_PATH . '/config/env.php';
+    $env_path    = isset($_SERVER['HTTP_HOST']) && isset($envMapping[$_SERVER['HTTP_HOST']])
+    ? $envMapping[$_SERVER['HTTP_HOST']] . '/'
+    : '';
+
+    //path for unit testing configuration
+    if(PHP_SAPI == 'cli') {
+        if(strpos($_SERVER['argv'][0], 'phpunit') !== FALSE) {
+            // echo 'aaaa'; var_dump($_SERVER['argv'][0]);
+            $env_path = 'testing';
+        }
+            // or
+            // if($_SERVER['argv'][0] == '/usr/bin/phpunit') { echo 'aaaasssss'; }
+    }
+    /**
+     * Read the configuration
+     */
+    if ($env_path != '' && file_exists(APP_PATH . '/config/' . $env_path . '/config.php')) {
+        $env    = rtrim($env_path, '/');
+        $config = include APP_PATH . '/config/' . $env_path . '/config.php';
+        // echo APP_PATH . '/config/' . $env_path . "config.php";
+    } else {
+        $env    = $default_env;
+        $config = include APP_PATH . '/config/config.php';
+        // echo 'yyyy';
+    }
+
+    // Define ENV
+    defined('ENV') || define('ENV', $env);
+
+    /**
+     * Read the configuration
+     */
+    // $config = include __DIR__ . "/../app/config/config.php";
 
     /**
      * Read auto-loader
      */
-    include APP_PATH . "/app/config/loader.php";
+    include __DIR__ . "/../app/config/loader.php";
 
     /**
      * Read services
      */
-    include APP_PATH . "/app/config/services.php";
+    include __DIR__ . "/../app/config/services.php";
+
+    require APP_PATH . '/helpers/constant.php';
 
     /**
      * Handle the request
